4e82d6e8c729747b6f2e7266af72290683f1ac06
Yujin <yjin.jeong982@gmail.com>
2025-11-04 15:25:16 +0900

    feature: add navigation

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,10 @@
+import Navigation from "../components/navigation";
+
 export default function NotFound() {
-  return <h1>404 Not Found</h1>;
+  return (
+    <div>
+      <Navigation />
+      <h1>Not found!</h1>
+    </div>
+  );
 }
